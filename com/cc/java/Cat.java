a81c0870774477095d1a1d70c533bb68b195b950
Sabine.Eitel <Seit201@aol.com>
2022-11-23 12:01:00 +0100

    get/set

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -13,23 +13,25 @@ public class Cat {
               this.age = age;
        }
 
-       public String tellYourName()
-       {
-              return this.name;
-       }
-       public String tellYourFurColor()
-       {
-              return this.furColor;
-       }
-       public int tellYourAge()
-       {
-              return this.age;
-       }
-public void changeName(String name) {
+
+public String getName() {
+       return name; 
+}
+public void setName(String name) {
        this.name = name;
 }
-
-
+public String getFurColor() {
+       return furColor;
+}
+public void setFurColor(String furColor) {
+       this.furColor = furColor;
+}
+public int getAge() {
+       return age;
+}
+public void setAge(int age) {
+       this.age = age;
+}
 
 public void tellYourAddress(){
    // Aufruf der Methode output() in der KLASSE App
