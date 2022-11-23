c7b3747459e354f10f0123a26c743ae7fc439432
Sabine.Eitel <Seit201@aol.com>
2022-11-23 08:44:45 +0100

    update

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,6 +2,15 @@ package com.cc.java;
 
 public class Cat {
     
+
+public String name;
+public String furColor;
+public int    age;
+
+
+
+
+
 public void tellYourAddress(){
    // Aufruf der Methode output() in der KLASSE App
    // this ist die Referenz des Objektes auf sich selbst
