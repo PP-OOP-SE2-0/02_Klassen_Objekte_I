764a53ac5062e6d66c49e1f66a09705f4aa520c0
Sabine.Eitel <Seit201@aol.com>
2022-11-23 09:13:06 +0100

    update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,27 +5,44 @@ public class App {
     public static void main(String[] args) {
 
         /** Typkonvertierung  */
-        App.output(String.valueOf(2));  // int --> String
-        App.output(Integer.toString(2));  // int --> String
-        App.output("Das ist eine Ziffer: " + 2);  // int --> String
+        // App.output(String.valueOf(2));  // int --> String
+        // App.output(Integer.toString(2));  // int --> String
+        // App.output("Das ist eine Ziffer: " + 2);  // int --> String
 
         output("------------------------");
         
         // neues Objekt wird erzeugt, Adresse wird cat zugewiesen
-        Cat cat = new Cat();
+    Cat cat = new Cat();
+    cat.name = "Grizabella";
+    cat.furColor = "white";
+    cat.age = 29;
+    
+    // output(cat.name);
+    // output(cat.furColor);
+    // output(String.valueOf(cat.age));
+
+
+
+
+
+
+
+    output(cat.tellYourName());
+
+
         // Methode output befindet sich in der KLASSE App
-        App.output("Blick von aussen: "+ cat); 
+        // App.output("Blick von aussen: "+ cat); 
         // Methode tellYourAddress() befindet sich im OBJEKT cat 
-        cat.tellYourAddress();
+        // cat.tellYourAddress();
     
         output("------------------------");
 
         // neues Objekt wird erzeugt, Adresse wird cat1 zugewiesen
-        Cat cat1 = new Cat();
+        // Cat cat1 = new Cat();
         // Methode output befindet sich in der KLASSE App
-        output("Blick von aussen: "+ cat1); 
+        // output("Blick von aussen: "+ cat1); 
         // Methode tellYourAddress() befindet sich im OBJEKT cat1 
-        cat1.tellYourAddress();
+        // cat1.tellYourAddress();
 
     }
 
