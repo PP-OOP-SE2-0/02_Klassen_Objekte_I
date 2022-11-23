1cdfc1f1e18567d871e4fee6d685d3a6513e0c83
Sabine.Eitel <Seit201@aol.com>
2022-11-23 10:42:12 +0100

    constructor

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,24 +1,30 @@
 package com.cc.java;
 
-public class Cat {
-    
+public class Cat { 
 
-public String name = "nobody";
-public String furColor;
-public int    age;
+       public String name;
+       public String furColor;
+       public int    age;
+      
 
-public String tellYourName()
-{
-       return this.name;
-}
-public String tellYourFurColor()
-{
-       return this.furColor;
-}
-public int tellYourAge()
-{
-       return this.age;
-}
+       public Cat(String name, String furColor, int age) {
+              this.name = name;
+              this.furColor = furColor;
+              this.age = age;
+       }
+
+       public String tellYourName()
+       {
+              return this.name;
+       }
+       public String tellYourFurColor()
+       {
+              return this.furColor;
+       }
+       public int tellYourAge()
+       {
+              return this.age;
+       }
 
 
 
